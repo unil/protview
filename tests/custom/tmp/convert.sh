bc27471510d72e16949a10ac294ebefa520eb6d0
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-04 17:10:23 +0200

    cleaning

--- tests/custom/tmp/convert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Applications/Inkscape.app/Contents/Resources/script "/Volumes/FILES/smeier/Sites/protein-visualizer/app/public/protein.svg" --export-png="/Volumes/FILES/smeier/Sites/protein-visualizer/app/public/protein.png"
