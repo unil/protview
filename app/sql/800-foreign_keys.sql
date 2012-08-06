771a608e4f7cd1b910f7169efa08a8b6f43ed1c3
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-06 11:29:36 +0200

    sql wip

--- app/sql/800-foreign_keys.sql
+++ app/sql/800-foreign_keys.sql
@@ -1,2 +1,22 @@
-ALTER TABLE `sites`
-  ADD CONSTRAINT `prtein_fk` FOREIGN KEY (`protein_id`) REFERENCES `proteins` (`id`);
+ALTER TABLE `subunits`
+  ADD CONSTRAINT `protein_fk` FOREIGN KEY (`protein_id`) REFERENCES `proteins` (`id`);
+  
+ALTER TABLE `peptides`
+  ADD CONSTRAINT `subunit_fk` FOREIGN KEY (`subunit_id`) REFERENCES `subunits` (`id`);
+  
+ALTER TABLE `regions`
+  ADD CONSTRAINT `peptide_fk` FOREIGN KEY (`peptide_id`) REFERENCES `peptides` (`id`);
+
+ALTER TABLE `amino_acids`
+  ADD CONSTRAINT `region_fk` FOREIGN KEY (`region_id`) REFERENCES `regions` (`id`);
+  
+ALTER TABLE `representations`
+  ADD CONSTRAINT `peptide_fk` FOREIGN KEY (`peptide_id`) REFERENCES `peptides` (`id`);
+
+ALTER TABLE `geometries`
+  ADD CONSTRAINT `representation_fk` FOREIGN KEY (`representation_id`) REFERENCES `representations` (`id`);
+  ADD CONSTRAINT `region_fk` FOREIGN KEY (`region_id`) REFERENCES `regions` (`id`);
+
+ALTER TABLE `coordinates`
+  ADD CONSTRAINT `geometry_fk` FOREIGN KEY (`geometry_id`) REFERENCES `geometries` (`id`);
+  ADD CONSTRAINT `amino_acid_fk` FOREIGN KEY (`amino_acid_id`) REFERENCES `amino_acids` (`id`);
