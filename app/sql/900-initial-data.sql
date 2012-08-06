34877f1e8121bcd4a708c4b5aacdf3db442da57e
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-06 12:49:49 +0200

    sql file fixed

--- app/sql/900-initial-data.sql
+++ app/sql/900-initial-data.sql
@@ -2,5 +2,4 @@
 /*Only executed for test purpose in non production environment */
 if (xContext::$profile != 'production') { ?>
 
-INSERT INTO `proteins`(id, name, species, note) VALUES (1,'protein 1', null, ''),(2,'protein 2', null, null);
-<?php }?>
+INSERT INTO `proteins`(id, name, species, note) VALUES (1,'protein 1', null, ''),(2,'protein 2', null, null);<?php }?>
