7c89d01c53d47cde06e0044a24074159b8e7da06
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-06 12:43:07 +0200

    test data wip

--- app/sql/100-proteins.sql
+++ app/sql/100-proteins.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS proteins;
 CREATE TABLE proteins (
     id INT NOT NULL AUTO_INCREMENT,
     name TEXT COLLATE utf8_unicode_ci NOT NULL,
-    species VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
-    nonte TEXT COLLATE utf8_unicode_ci NOT NULL,
+    species VARCHAR(255) COLLATE utf8_unicode_ci,
+    note TEXT COLLATE utf8_unicode_ci,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
