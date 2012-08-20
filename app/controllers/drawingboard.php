f38b9798161beb60e2238c87d6c79e1164e4c96b
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-20 20:26:59 +0200

    add svg export

--- app/controllers/drawingboard.php
+++ app/controllers/drawingboard.php
@@ -5,13 +5,35 @@ class DrawingBoardController extends RESTController {
 		$data = array();
 		return xView::load('drawingboard/start', $data, $this->meta)->render();
 	}
-	
-	function convertAction() {
-		require_once(xContext::$basepath.'/lib/protview/protview/graph/SVGConverter.php');
-		
-		$png = SVGConverter::toPNG($svgFile);
-		
+
+	function exportAction() {
+		$png = null;
+
+		$svgContent = $this->params['svg'];
+
+		if ($svgContent != null) {
+			require_once(xContext::$basepath.'/lib/protview/protview/graph/SVGConverter.php');
+			$cssFile = xContext::$basepath. '/public/a/css/' . $this->params['css'];;
+
+
+			$svg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg">';
+
+			$style = '<style type="text/css"><![CDATA[';
+
+			if (file_exists($cssFile))
+				$style .= file_get_contents($cssFile);
+
+			$style .= ']]></style>';
+
+			$svg .= $style;
+			$svg .= $svgContent;
+			$svg .= '</svg>';
+
+
+			$png = SVGConverter::SVGStreamToPNG($svg);
+		}
 		$data = array('png' => $png);
+
 		return xView::load('drawingboard/export', $data, $this->meta)->render();
 	}
 }
