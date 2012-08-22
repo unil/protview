cf5fd6e2cca4b56a055a8da9c7dd49dc7ebdb022
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-22 16:43:19 +0200

    bug fixed

--- app/public/assets/js/protview/structure/view/NewProteinView.js
+++ app/public/assets/js/protview/structure/view/NewProteinView.js
@@ -5,6 +5,19 @@ ProtView.Structure.View.NewProteinView = ProtView.Core.View.extend({
 	submitForm : function(e) {
         e.preventDefault();
         this.controller.save();
-        $('#new-protein-dialog').jqxWindow('close');
-    }
+        
+        
+        this.model.on('change', this.update, this);
+        console.log(model);
+        console.log(this.model);
+    },
+	update : function() {
+		proteinId = this.model.get('xinsertid');
+		
+		if (proteinId != null && proteinId != 0) {
+			Application.CONTEXT.navigate("view/" + proteinId + "/0");
+			this.unload();
+			$('#new-protein-dialog').jqxWindow('close');
+		}
+	}
 });
