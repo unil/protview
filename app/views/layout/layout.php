3a7b99e7e3285ba588970a4e5af83170592b2947
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-24 15:40:36 +0200

    bug fixed

--- app/views/layout/layout.php
+++ app/views/layout/layout.php
@@ -46,7 +46,7 @@ class LayoutLayoutView extends xView {
             	xUtil::url('a/js/protview/core/View.js'),
             	xUtil::url('a/js/protview/DrawBoard.js'),
             	xUtil::url('a/js/protview/Structure.js'),
-            	xUtil::url('a/js/protview/Application/Router.js'),
+            	xUtil::url('a/js/protview/application/Router.js'),
             	xUtil::url('a/js/protview/application/Mediator.js'),
             	xUtil::url('a/js/protview/application/Context.js'),
             	xUtil::url('a/js/protview/application/Sandbox.js'),
