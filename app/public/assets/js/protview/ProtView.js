498c05a6085c11710d6829af878b20d9602ca22b
Stefan Meier <stefan.r.meier@unil.ch>
2012-08-24 15:37:31 +0200

    xfm update

--- app/public/assets/js/protview/ProtView.js
+++ app/public/assets/js/protview/ProtView.js
@@ -1,7 +1,12 @@
 /**
+ * Provides the base ProtView class
+ *
  * Requires at least (in the following order) : jQuery 1.7.2 underscore 1.3.3
  * backbonejs 0.9.2 jQuery SVG 1.4.5 jQuery SVG Dom (same version as jQuery SVG)
- */
+ *
+ @module ProtView
+**/
+
 (function($, exports) {
 	window.ProtView = {
 	};
