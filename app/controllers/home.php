9d046988b67b1f73401c6a563d9799063d3978d9
Stefan Meier <stefan.r.meier@unil.ch>
2012-09-03 17:07:16 +0200

    add comments

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,7 +1,19 @@
 <?php
-
+/**
+ * Home controller (callend on application startup)
+ *
+ * @package controllers
+ * @author Stefan Meier
+ * @version 20120903
+ *
+ */
 class HomeController extends xWebController {
 
+	/**
+	 * Gets the home page
+	 * 
+	 * @return \views\home\HomeHomeView
+	 */
     function defaultAction() {
     	$data = array();
         return xView::load('home/home', $data)->render();
--- app/controllers/menubar.php
+++ app/controllers/menubar.php
@@ -1,15 +1,40 @@
 <?php
-
+/**
+ * Menubar controller
+ *
+ * @package controllers
+ * @author Stefan Meier
+ * @version 20120903
+ *
+ */
 class MenubarController extends RESTController {
+	/**
+	 * Returns a 403 exception as defaultAction is not available
+	 *
+	 * @returns xException
+	 */
 	function defaultAction() {
-
+		throw new xException("Method not allowed", 403);
 	}
 
+	/**
+	 * Gets the 'New protein' dialog
+	 * 
+	 * @return \views\menubar\NewproteinView
+	 */
 	function newproteinAction() {
 		$data = array();
 		return xView::load('menubar/newprotein', $data, $this->meta)->render();
 	}
 
+	/**
+	 * Gets the 'open Representation' dialog
+	 * 
+	 * Retrieves the list of all representations from database
+	 * and sends this information as param to the view
+	 *
+	 * @return \views\menubar\OpenrepresentationView
+	 */
 	function openrepresentationAction() {
 		$representations = array();
 			
