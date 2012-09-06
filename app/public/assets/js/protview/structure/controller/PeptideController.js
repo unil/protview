c55d08f310444786724aaa1348820ff6b4cd5d82
Stefan Meier <stefan.r.meier@unil.ch>
2012-09-06 11:09:11 +0200

    api doc updated

--- app/public/assets/js/protview/structure/controller/PeptideController.js
+++ app/public/assets/js/protview/structure/controller/PeptideController.js
@@ -1,8 +1,35 @@
+/**
+ * 
+ * Peptide controller
+ * 
+ * 
+ * @module Structure
+ * @namespace Structure.Controller
+ * @class PeptideController
+ * @extends Core.Controller
+ * 
+ * @author Stefan Meier
+ * @version 20120903
+ * 
+ */
 ProtView.Structure.Controller.PeptideController = ProtView.Core.Controller.extend( {
+	/**
+	 * Initalizes model and calls parent constructor
+	 * 
+	 * @method initialize
+	 * @constructor
+	**/
 	init: function() {		
 		var model = new ProtView.Structure.Model.Peptide();
 		this._super(model);
 	},
+	/**
+	 * Overrides fetch method in order to handle protein specifiec model attributes
+	 * 
+	 * @method fetch
+	 * @param {int} protein_id
+	 * @return {Object} server response
+	**/
 	fetch : function(protein_id) {
 		var ret = null,
 		model = this.model;
--- app/public/assets/js/protview/structure/controller/ProteinController.js
+++ app/public/assets/js/protview/structure/controller/ProteinController.js
@@ -1,4 +1,24 @@
+/**
+ * 
+ * Protein controller
+ * 
+ * 
+ * @module Structure
+ * @namespace Structure.Controller
+ * @class ProteinController
+ * @extends Core.Controller
+ * 
+ * @author Stefan Meier
+ * @version 20120903
+ * 
+ */
 ProtView.Structure.Controller.ProteinController = ProtView.Core.Controller.extend( {
+	/**
+	 * Initalizes model and calls parent constructor
+	 * 
+	 * @method initialize
+	 * @constructor
+	**/
 	init: function() {
 		var model = new ProtView.Structure.Model.Protein();
 		this._super(model);
