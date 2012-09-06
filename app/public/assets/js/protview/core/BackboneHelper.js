24d75edabf0e83539e10039d878d3213822c9697
Stefan Meier <stefan.r.meier@unil.ch>
2012-09-06 14:46:19 +0200

    delete unused classes

--- app/public/assets/js/protview/core/BackboneHelper.js
+++ app/public/assets/js/protview/core/BackboneHelper.js
@@ -1,7 +1,33 @@
+/**
+ * 
+ * Core Helper class for Backbone fetching/saving
+ * 
+ * 
+ * @module Core
+ * @namespace Core
+ * @class BackboneHelper
+ * 
+ * @author Stefan Meier
+ * @version 20120903
+ * 
+ */
 ProtView.Core.BackboneHelper = Class.extend( {
+	/**
+	 * Constructor
+	 * 
+	 * @constructor
+	 * @param resource
+	 */
 	init: function(collection) {
 		this.collection = collection;
 	},
+	/**
+	 * Fetches data from Server
+	 * 
+	 * @param {function} callback
+	 * @param {Object} fetchOptions
+	 * @method fetch
+	 */
 	fetch : function(callback, options) {
 		var ret = this.collection;
 		this.collection.fetch({
--- app/public/assets/js/protview/core/MainController.js
+++ app/public/assets/js/protview/core/MainController.js
@@ -1,16 +1,40 @@
+/**
+ * 
+ * AbstractMainController, implements main functionalities
+ * 
+ * 
+ * @module Core
+ * @namespace Core
+ * @class MainController
+ * 
+ * @author Stefan Meier
+ * @version 20120903
+ * 
+ */
 ProtView.Core.MainController = Class.extend( {
-	controller : null,
+	/**
+	* Contains all loaded resources
+	*
+	* @property stack
+	* @type Object
+	* @default null
+	**/
 	stack : {},
-	currentResource : null,
-	init: function() {
-	},
+	/**
+	 * Gets controller from stack
+	 * 
+	 * @param controllerName
+	 * @return {Core.Controller} controller
+	 */
 	getController : function(resource) {
 		return this.stack[resource];
 	},
-	load : function(resource) {
-	},
-	unload: function (resource) {
-	},
+	/**
+	 * Unloads all loaded resources
+	 * 
+	 * @method unload
+	 * @param {Object} resource
+	 */
 	unloadAll : function() {
 		var stack = this.stack;
 		for (var el in stack) {
